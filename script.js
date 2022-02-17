a3a77e6173dc3b241426cdc5a9ab3e2bd9ca6e40
Danila Simonov <ek@ek-mail.ru>
2022-02-17 17:11:48 +0300

    feat: js cleaning, variables renaming, code-formatting

--- script.js
+++ script.js
@@ -1,203 +1,156 @@
-window.onload = () =>{
-// Company logo
-document.getElementById("imgLogo").src = "https://sun1-23.userapi.com/AMRzSyPgEmDTGxBS2NS2q3OtocpKrVCeWIBzHA/iDhbcrxZNDc.jpg?ava=1";
-// Employee avatar
-document.getElementById("employeeAvatar").src = "https://sun9-88.userapi.com/impf/ICTO9JIaPMmmNyo72hAggilHYNKBdCg9n8QycQ/DIp-hZqTJ3g.jpg?size=762x1080&quality=96&sign=2b1a52ee5467368febe46c9813f052ad&type=album";
-// Employee firstname
-document.getElementById("employeeFirstName").innerHTML = "Arteom";
-// amount excluding 5%
-let price = 0
-// persent
-let percent = 0
-// full price
-let fullPrice = 0
-// Buttons AED
-const tipsAmounts = [5, 10, 15, 20]
-// Star rating
-let rating = document.querySelector('.rateStart')
-// array with images and text of compliments
-const arrCompliments = [
-    {textFirst: "Nice",
-    textSecond: "atmosphere",
-    imgLink: "https://e-tips.me/assets/review-options/atmosphere.svg",
-    selected: false
+const COMISSION_PERCENT = 0.05;
+const COMPANY_LOGO_URL = 'https://sun1-23.userapi.com/AMRzSyPgEmDTGxBS2NS2q3OtocpKrVCeWIBzHA/iDhbcrxZNDc.jpg?ava=1';
+const EMPLOYEE_AVATAR_URL =
+    'https://sun9-88.userapi.com/impf/ICTO9JIaPMmmNyo72hAggilHYNKBdCg9n8QycQ/DIp-hZqTJ3g.jpg?size=762x1080&quality=96&sign=2b1a52ee5467368febe46c9813f052ad&type=album';
+const EMPLOYEE_NAME = 'Arteom';
+const DEFAULT_TIPS_AMOUNTS = [5, 10, 15, 20];
+const COMPLIMENTS = [
+    {
+        title: 'Nice',
+        description: 'atmospere',
+        icon: 'https://e-tips.me/assets/review-options/atmosphere.svg',
     },
-    {textFirst: "Excellent",
-    textSecond: "food",
-    imgLink: "https://e-tips.me/assets/review-options/food.svg",
-    selected: false
+    {
+        title: 'Excellent',
+        description: 'food',
+        icon: 'https://e-tips.me/assets/review-options/food.svg',
     },
-    {textFirst: "Perfect",
-    textSecond: "service",
-    imgLink: "https://e-tips.me/assets/review-options/service.svg",
-    selected: false
+    {
+        title: 'Perfect',
+        description: 'service',
+        icon: 'https://e-tips.me/assets/review-options/service.svg',
     },
-]
-const tipInputOut = document.querySelector('#amountInput')
-const inputNumber = document.querySelector('#firstNumber')
-const AED = document.querySelector('#secondNumber')
+];
 
-//AED Visible
-const input = () => {    
-    if (tipInputOut.value){
-        inputNumber.innerHTML = tipInputOut.value
-        AED.classList.add('AEDvisible')
-        tipInputOut.classList.remove('empty')
-        price = tipInputOut.value
-    }else{
-        inputNumber.value = ''
-        tipInputOut.classList.add('empty')
-        AED.classList.remove('AEDvisible')
-        price = 0 
+window.addEventListener('DOMContentLoaded', () => {
+    const $imgLogo = document.querySelector('#imgLogo');
+    const $employeeAvatar = document.querySelector('#employeeAvatar');
+    const $employeeFirstName = document.querySelector('#employeeFirstName');
+    const $tipInputOut = document.querySelector('#amountInput');
+    const $hiddenTipAmountLabel = document.querySelector('#firstNumber');
+    const $AED = document.querySelector('#secondNumber');
+    const $predefinedTipsButtons = document.querySelector('#predefinedTipsButtons');
+    const $payComissionYourselfCheckbox = document.querySelector('#payComissionYourselfCheckbox');
+    const $spanComissionValue = document.querySelector('#comission');
+    const $divFeatures = document.querySelector('#divFeatures');
+    const $payService = document.querySelector('#payService');
+
+    $imgLogo.src = COMPANY_LOGO_URL;
+    $employeeAvatar.src = EMPLOYEE_AVATAR_URL;
+    $employeeFirstName.innerText = EMPLOYEE_NAME;
+
+    // tip amount with comission
+    let tipValue = 0;
+    let comissionValue = 0;
+    const tipsAmounts = DEFAULT_TIPS_AMOUNTS.slice();
+
+    // array with images and texts of compliments
+    const arrCompliments = COMPLIMENTS.map(comp => ({
+        textFirst: comp.title,
+        textSecond: comp.description,
+        imgLink: comp.icon,
+        selected: false,
+    }));
+
+    function buildElement(tag, className, text) {
+        const el = document.createElement(tag);
+        el.classList.add(...className.split(' '));
+        if (text) {
+            el.innerText = text;
+        }
+        return el;
     }
-    percentСounter()
-}
 
-if (tipInputOut) {
-    tipInputOut.addEventListener('input', input)
-    }
-
-const priceChance = (newPrice) => {
- price = newPrice
- tipInputOut.value = newPrice
- inputNumber.innerHTML = newPrice
- AED.classList.add('AEDvisible')
- tipInputOut.classList.remove('empty')
- percentСounter()
-}
-
-tipsAmounts.forEach((elem) => {
-    const button = document.createElement("button");
-    button.classList.add('AEDbutton')
-    button.appendChild(document.createTextNode(`${elem} AED`));
-    button.onclick = () => priceChance(elem);
-    amountButtons.appendChild(button)
-    
-})
-//rating
-let ratingItem = document.querySelectorAll('.star')
-
-rating.onclick = function(e){
-    let target = e.target
-    if (target.classList.contains('star')){
-        removeClass(ratingItem, 'current-active')
-        target.classList.add('active','current-active')
-    }
-}
-
-rating.onmouseover = function(e) {
-    let target = e.target
-    if (target.classList.contains('star')){
-        removeClass(ratingItem, 'active')
-        target.classList.add('active')
-        mouseOverActiveClass(ratingItem)
-    }
-}
-
-rating.onmouseout = function() {
-    addClass(ratingItem,'active')
-    mouseOutActiveClass(ratingItem)
-}
-
-function removeClass(arr){
-    for(let i =0, iLen = arr.length; i < iLen; i++) {
-        for(let j = 1; j < arguments.length; j++){
-            ratingItem[i].classList.remove(arguments[j])
+    function updateHiddenTipLabel() {
+        if ($tipInputOut.value) {
+            $hiddenTipAmountLabel.innerText = $tipInputOut.value;
+            $AED.classList.add('AEDvisible');
+            $tipInputOut.classList.remove('empty');
+            tipValue = $tipInputOut.value;
+        } else {
+            $hiddenTipAmountLabel.value = '';
+            $tipInputOut.classList.add('empty');
+            $AED.classList.remove('AEDvisible');
+            tipValue = 0;
         }
     }
-}
 
-function addClass(arr){
-    for(let i =0, iLen = arr.length; i < iLen; i++) {
-        for(let j = 1; j < arguments.length; j++){
-            ratingItem[i].classList.add(arguments[j])
+    // AED Label
+    $tipInputOut.addEventListener('input', () => {
+        updateHiddenTipLabel();
+        calculateComission();
+    });
+
+    const setPredefinedTipValue = newPrice => {
+        $tipInputOut.value = newPrice;
+        updateHiddenTipLabel();
+        calculateComission();
+    };
+
+    tipsAmounts.forEach(elem => {
+        const button = buildElement('button', 'AEDButton', `${elem} AED`);
+        button.addEventListener('click', setPredefinedTipValue.bind(null, elem));
+        $predefinedTipsButtons.appendChild(button);
+    });
+
+    let starActive = null;
+
+    document.querySelectorAll('button.star').forEach(starButton => {
+        starButton.addEventListener('click', ({ target }) => {
+            if (starActive) {
+                starActive.classList.remove('active');
+            }
+            target.classList.add('active');
+            starActive = target;
+        });
+    });
+
+    // Service features / compliments
+    const renderButtons = () => {
+        //remove all existing buttons
+        $divFeatures.innerHTML = '';
+
+        //render new buttons
+        arrCompliments.forEach(elem => {
+            const button = buildElement('button', elem.selected ? 'features featuresSelected' : 'features');
+            const img = buildElement('img', 'icon');
+            const spanFirst = buildElement('span', 'first', elem.textFirst);
+            const spanSecond = buildElement('span', 'second', elem.textSecond);
+
+            img.src = elem.imgLink;
+
+            button.appendChild(img);
+            button.appendChild(spanFirst);
+            button.appendChild(spanSecond);
+
+            button.addEventListener('click', () => {
+                elem.selected = !elem.selected;
+                renderButtons();
+            });
+
+            $divFeatures.appendChild(button);
+        });
+    };
+
+    renderButtons();
+
+    function calculateComission() {
+        if (isNaN(tipValue) || !tipValue) {
+            $payComissionYourselfCheckbox.style.display = 'none';
+            comissionValue = 0;
+        } else {
+            tipValue = +tipValue;
+            comissionValue = (tipValue * COMISSION_PERCENT).toFixed(2);
+            $spanComissionValue.innerText = comissionValue;
+            $payComissionYourselfCheckbox.style.display = 'block';
         }
     }
-}
 
-function mouseOverActiveClass(arr){
-    for(let i=0, iLen = arr.length; i<iLen; i++){
-        if(arr[i].classList.contains('active')){
-            break
-        }else{
-            arr[i].classList.add('active')
+    function checkPrice() {
+        if ($payService.checked) {
+            fullPrice = +tipValue + +comissionValue;
+        } else {
+            fullPrice = tipValue;
         }
     }
-}
-
-function mouseOutActiveClass(arr){
-    for(let i= arr.length-1; i >= 1; i--){
-        if(arr[i].classList.contains('current-active')){
-            break
-        }else{
-            arr[i].classList.remove('active')
-        }
-    }
-}
-
-// Service features / compliments
-const renderButtons = () => {
-const divFeatures = document.getElementById('divFeatures')
-    //remove all existing buttons
-    while(divFeatures.firstChild) {
-        divFeatures.removeChild(divFeatures.firstChild)
-    }
-    
-    //render new buttons
-arrCompliments.forEach((elem) => {
-    const buttonCompliments = document.createElement('button')
-    const imgCompliments = document.createElement("img")
-    buttonCompliments.classList.add('features')
-    imgCompliments.classList.add('icon')
-    imgCompliments.src = elem.imgLink
-    buttonCompliments.appendChild(imgCompliments)
-    const spanComplimentsFirst = document.createElement("span")
-    spanComplimentsFirst.classList.add('first')
-    spanComplimentsFirst.innerHTML = elem.textFirst
-    buttonCompliments.appendChild(spanComplimentsFirst)
-    const spanComplimentsSecond = document.createElement("span")
-    spanComplimentsSecond.classList.add('second')
-    spanComplimentsSecond.innerHTML = elem.textSecond
-    buttonCompliments.appendChild(spanComplimentsSecond)
-    if(elem.selected) {
-        buttonCompliments.classList.add('featuresSelected')
-    } else {
-        buttonCompliments.classList.remove('featuresSelected')
-    }
-    buttonCompliments.onclick = () => {
-        elem.selected = !elem.selected
-        renderButtons()
-    }
-    divFeatures.appendChild(buttonCompliments)
-
-})
-}
-renderButtons()
-
-//Persent
-const lablePay =  document.getElementById("lablePay")
-let spanPercent = document.getElementById("percent")
-
-
-function percentСounter () {
-    if(+(price) === 0 || price === ""){
-        lablePay.style.display = 'none'
-        percent = 0
-    }else{
-        price = +price
-        percent = (price * 0.05).toFixed(2)
-        spanPercent.innerHTML = percent
-        lablePay.style.display = 'block'
-    }
-}
-
-// Full price
-const payServis = document.getElementById('payServis')
-function checkPrice () {
-    if (payServis.checked){
-        fullPrice = (+price) + (+percent)
-    }else{
-        fullPrice =  price
-    }
-}
-}
+});
